df769228171d9dfdcff24dd2e8824f0645d600cc
ankit5183 <ankit2002sofipur@gmail.com>
2025-12-11 20:31:58 +0530

    Update Register.jsx

--- finance_frontend/src/Pages/Register.jsx
+++ finance_frontend/src/Pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URL;
 
 function Register() {
   const navigate = useNavigate();
