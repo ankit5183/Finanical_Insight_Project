2c8ba57bbb3cdfb5563fdbe93a9995917ade85a0
ankit5183 <ankit2002sofipur@gmail.com>
2025-12-12 11:35:39 +0530

    Update WeeklyExpense.jsx

--- finance_frontend/src/Pages/WeeklyExpense.jsx
+++ finance_frontend/src/Pages/WeeklyExpense.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { API_URL } from "../config";
 
-const API_URL = process.env.REACT_APP_API_URL;
 
 function WeeklyExpense() {
   const [year, setYear] = useState("");
@@ -25,7 +26,7 @@ function WeeklyExpense() {
     const url = `${API_URL}/api/expense/weekly?year=${year}&month=${month}&day=${day}`;
 
     try {
-      const response = await fetch(url, {
+      const response = await axios.get(url, {
         method: "GET",
         headers: {
           Authorization: "Bearer " + token,
