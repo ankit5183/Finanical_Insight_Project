3699c31051643d8f744242fbd28e6e8ce2c30365
ankit5183 <ankit2002sofipur@gmail.com>
2025-12-12 12:07:45 +0530

    Update BudgetPage.jsx

--- finance_frontend/src/Pages/BudgetPage.jsx
+++ finance_frontend/src/Pages/BudgetPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { API_URL } from "../config";
+import { API_URL } from "../Config";
 
 function BudgetPage() {
   const [year, setYear] = useState("");
