b508acf8e094e8ccfd1b4e8bff724a39659e1b13
ankit5183 <ankit2002sofipur@gmail.com>
2025-12-12 12:09:48 +0530

    Update Register.jsx

--- finance_frontend/src/Pages/Register.jsx
+++ finance_frontend/src/Pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { API_URL } from "../config";
+import { API_URL } from "../Config";
 
 function Register() {
   const navigate = useNavigate();
