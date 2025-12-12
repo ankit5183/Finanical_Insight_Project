96ad1a2a9e0fb2714b31ee8d2ea4acbdf790d4d3
ankit5183 <ankit2002sofipur@gmail.com>
2025-12-12 12:08:52 +0530

    Update Login.jsx

--- finance_frontend/src/Pages/Login.jsx
+++ finance_frontend/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { API_URL } from "../config";
+import { API_URL } from "../Config";
 
 function Login() {
   const navigate = useNavigate();
