4645f4e361b5a569576f4663e0f84bd8177d313c
ankit5183 <ankit2002sofipur@gmail.com>
2025-12-19 20:15:33 +0530

    Update UserService.java

--- src/main/java/Project_maker/finance_insight/user_model/UserService.java
+++ src/main/java/Project_maker/finance_insight/user_model/UserService.java
@@ -9,20 +9,26 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository,
+                       BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-     public User registerUser(User user) {
-    if (userRepository.findByEmail(user.getEmail()) != null) {
-        throw new RuntimeException("Email already in use");
+    public User registerUser(User user) {
+
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new IllegalArgumentException("Email already in use");
+        }
+
+        user.setEmail(user.getEmail().toLowerCase());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
     }
-    user.setPassword(passwordEncoder.encode(user.getPassword()));
-    return userRepository.save(user);
-}
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 }
+
