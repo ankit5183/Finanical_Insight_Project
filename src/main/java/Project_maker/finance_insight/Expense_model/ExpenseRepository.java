df7cec8740709046296b7f0fb62bf20046571e74
ankit5183 <ankit2002sofipur@gmail.com>
2025-12-22 13:29:16 +0530

    Update ExpenseRepository.java

--- src/main/java/Project_maker/finance_insight/Expense_model/ExpenseRepository.java
+++ src/main/java/Project_maker/finance_insight/Expense_model/ExpenseRepository.java
@@ -2,20 +2,22 @@ package Project_maker.finance_insight.Expense_model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
-    // 1. Basic find by email
     List<Expense> findByUserEmail(String email);
-     
+
+    @Modifying
+    @Transactional
     void deleteByIdInAndUserEmail(List<Long> ids, String userEmail);
-    // 2. Used for date range filtering
+
     List<Expense> findByUserEmailAndDateBetween(String email, LocalDate startDate, LocalDate endDate);
 
-    // 3. Total Monthly Spent (for Budget Status)
     @Query("SELECT COALESCE(SUM(e.amount), 0.0) FROM Expense e " +
            "WHERE e.userEmail = :email " +
            "AND MONTH(e.date) = :month " +
@@ -24,11 +26,9 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
                              @Param("month") int month, 
                              @Param("year") int year);
 
-    // 4. Total Expense for Dashboard
     @Query("SELECT COALESCE(SUM(e.amount), 0.0) FROM Expense e WHERE e.userEmail = :email")
     Double getTotalExpense(@Param("email") String email);
 
-    // 5. Category Wise Summary for Pie Chart
     @Query("SELECT e.category, SUM(e.amount) " +
            "FROM Expense e " +
            "WHERE e.userEmail = :email " +
